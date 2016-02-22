92681deac4c11bec83b2ed8f71033f622d8c426f
RAMKRISHNA VERMA <ramkrishna.register@gmail.com>
2016-02-23 02:40:11 +0530

    slight changes in developers file
    Change some contents in developers.php file

--- pages/developers.php
+++ pages/developers.php
@@ -23,77 +23,12 @@
 			</div>
 			<div class="col-md-6">
 				<div class="alert alert-info">
-					<h3>Nitin Bhandari</h3>
+					<h3>Aman Singh</h3>
 					<strong>Email - </strong>
 					<a href="#" class="alert-link">bnitin92@gmail.com</a>
 				</div>				
 			</div>
 		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div class="alert alert-warning">
-					<h3>Shravani Dalvi</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">shra191993@gmail.com</a>
-				</div>				
-			</div>
-			<div class="col-md-6">
-				<div class="alert alert-danger">
-					<h3>Saheeka Society Doruge</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">saheekasd@gmail.com</a>
-				</div>				
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div class="alert alert-info">
-					<h3>Sudeep Maity</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">sudeep.nitg@gmail.com</a>
-				</div>				
-			</div>
-			<div class="col-md-6">
-				<div class="alert alert-success">
-					<h3>Gayatri Kudchadker</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">gayatri.kud@gmail.com</a>
-				</div>				
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div class="alert alert-success">
-					<h3>Amit Kumar Rawat</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">amitrawat29292@gmail.com</a>
-				</div>				
-			</div>			
-			<div class="col-md-6">
-				<div class="alert alert-info">
-					<h3>Megha Patel</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">pmegha1314@gmail.com</a>
-				</div>				
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-md-6">
-				<div class="alert alert-warning">
-					<h3>Saish Naik</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">saishnaik85@yahoo.in</a>
-				</div>				
-			</div>			
-			<div class="col-md-6">
-				<div class="alert alert-danger">
-					<h3>Sneha Kamble</h3>
-					<strong>Email - </strong>
-					<a href="#" class="alert-link">sneha311093@gmail.com</a>
-				</div>				
-			</div>
-		</div>
-	</div>
 	<?php include_once "../page_components/js_links.php"; ?>
 </body>
 </html>
